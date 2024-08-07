f6306a735c9ad01ec46d74c053e8677b706adc39
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-08-07 21:39:27 +0530

    updated scripts

---
+++ week2/basicScripts2.asv
@@ -0,0 +1,8 @@
+% Basic MATLAB: Sanjay, 7/08/2024
+%Goal perform basic mathematics
+clc, clear all, format compact
+%parameters and variables
+%Execute
+20/5
+400*6
+pi*33
---
+++ week2/basicScripts3.m
@@ -0,0 +1,7 @@
+% Defining Variables: Sanjay, 7/08/2024
+%Goal: Define basic variables
+clc, clear all, format compact
+%parameters and variables
+fracture =25;
+location='Golden gate';
+needs_repair=true;
