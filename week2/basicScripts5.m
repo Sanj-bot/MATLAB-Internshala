e54ada34141541e7a96b1fa5253ea63b928d9e72
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-08-08 18:20:36 +0530

    bs5

---
+++ week2/basicScripts5.m
@@ -0,0 +1,14 @@
+%: Sanjay, 8/8/2024
+% matrix
+clc, clear all, format compact
+%defining matrix
+%a=eye(5)
+%b=ones(10)
+%c=eye(6)
+%d=c'
+horiz1=[1 2 3 4 5];
+veriz1=[1;2;3;4;5];
+matrix1=[1,2,3;4,5,6;7,8,9];
+time1=linspace(1,50,10);
+time2=1:2:50;
+matrix2=eye(10)
