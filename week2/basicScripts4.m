c5d8be3b7c4353b84770d04a42d1191a444fe10d
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-08-08 18:20:30 +0530

    bs4

---
+++ week2/basicScripts4.m
@@ -0,0 +1,9 @@
+%Arrays and vectors: Sanjay, 7/8/2024
+%learning about arrays and vectors
+clc, clear all, format compact
+%define array
+%1. manual input
+%time=[1 2 3 4 5];
+%distance=[1 2 3 4 2];
+%work=[1,2,3,4,5];
+%numbers=linspace(1,100,100);
