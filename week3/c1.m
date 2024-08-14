888ba9bed7e32ae461612ca108eeff113b278bb3
Sanjay Singh <sanjaygurjar741@gmail.com>
2024-08-14 22:23:08 +0530

    update of c1

--- week3/c1.m
+++ week3/c1.m
@@ -1,4 +1,18 @@
 %: Sanjay, /8/2024
-%
+%  Linear Algebrae Review
 clc, clear all, format compact
-%
+%Variables
+A=[5 7;-2 0.5;pi 8];            %3x2
+B=[10 -1; -2 -9; 7 1/2];        %3x2
+D=[-1;1];                       %2x1
+E=[7 22; -9 -1];                %2x2
+c=5;                            %scalar
+
+
+% Matrix Scalar Add/Subtract
+
+
+%Matrix Scalar Multiply Divide
+
+
+%Matrix Matrix Add/Subtract (the same dimensions!)
